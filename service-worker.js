4705bdebd8c4cfe30ee6d63abc81acf7fc267eda
adammwalsh <adammwalsh@icloud.com>
2026-07-24 14:33:40 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,14 @@
-const CACHE_NAME = "life-ledger-v2";
-
-const FILES = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json",
-    "./icon-192-maskable.png",
-    "./icon-512-maskable.png"
-];
+const CACHE = "life-ledger-v1";
 
 self.addEventListener("install", event => {
 
     self.skipWaiting();
 
-    event.waitUntil(
-
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES))
-
-    );
-
 });
 
 self.addEventListener("activate", event => {
 
-    event.waitUntil(
-
-        caches.keys().then(keys =>
-
-            Promise.all(
-
-                keys
-                .filter(key => key !== CACHE_NAME)
-                .map(key => caches.delete(key))
-
-            )
-
-        )
-
-    );
-
-    self.clients.claim();
+    event.waitUntil(clients.claim());
 
 });
 
@@ -49,8 +16,7 @@ self.addEventListener("fetch", event => {
 
     event.respondWith(
 
-        caches.match(event.request)
-        .then(response => response || fetch(event.request))
+        fetch(event.request).catch(() => caches.match(event.request))
 
     );
 
